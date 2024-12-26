c6ee0e419e5ef2101b0cbbff3012dfa7e0e59a3c
Weston Sankey <me@westonsankey.com>
2024-12-26 16:15:59 -0700

    add initial loading skeleton

--- src/app/components/BestStories.tsx
+++ src/app/components/BestStories.tsx
@@ -5,10 +5,11 @@ import { Modal } from "./Modal";
 import { fetchCommentSummary } from "@/api/commentSummary";
 import { Story } from "@/api/services/hackernews/types";
 import { CommentSummary } from "@/api/services/summary/schema";
+import { LoadingSkeleton } from "./LoadingSkeleton";
 
 export function BestStories({ stories }: { stories: Story[] }) {
   const [selectedStory, setSelectedStory] = useState<Story | null>(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [summary, setSummary] = useState<CommentSummary | null>(null);
 
   const handleSelectStory = async (story: Story) => {
@@ -16,7 +17,7 @@ export function BestStories({ stories }: { stories: Story[] }) {
     setLoading(true);
     const result = await fetchCommentSummary(story);
     setSummary(result);
-    setLoading(false);
+    // setLoading(false);
   };
 
   return (
@@ -34,8 +35,19 @@ export function BestStories({ stories }: { stories: Story[] }) {
       {selectedStory && (
         <Modal onClose={() => setSelectedStory(null)}>
           <h3 className="text-2xl font-bold">{selectedStory.title}</h3>
-          {loading && <div>Loading...</div>}
-          <div className="mt-6">{summary?.summary}</div>
+          {loading && (
+            <div className="mt-6">
+              <LoadingSkeleton />
+            </div>
+          )}
+          {!loading && <div className="mt-6">{summary?.summary}</div>}
+
+          <button
+            className="mt-6 border border-white hover:bg-slate-500 text-white px-4 py-2 rounded-md"
+            onClick={() => setSelectedStory(null)}
+          >
+            Close
+          </button>
         </Modal>
       )}
     </div>
---
+++ src/app/components/LoadingSkeleton.tsx
@@ -0,0 +1,29 @@
+export function LoadingSkeleton() {
+  return (
+    <div role="status" className="animate-pulse">
+      <div className="h-3 bg-gray-400 rounded-full dark:bg-gray-700 w-48 mb-5"></div>
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[400px] mb-3"></div>
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[550px] mb-3"></div>
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[430px] mb-3"></div>
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[490px] mb-3"></div>
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[450px] mb-3"></div>
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[400px] mb-5"></div>
+
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[550px] mb-3"></div>
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[430px] mb-3"></div>
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[490px] mb-3"></div>
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[400px] mb-3"></div>
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[450px] mb-3"></div>
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[550px] mb-5"></div>
+
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[550px] mb-3"></div>
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[430px] mb-3"></div>
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[490px] mb-3"></div>
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[400px] mb-3"></div>
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[450px] mb-3"></div>
+      <div className="h-2.5 bg-gray-400 rounded-full dark:bg-gray-700 max-w-[550px] mb-3"></div>
+
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+}
