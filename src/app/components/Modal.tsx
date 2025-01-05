69ef799dabc860bed028456302c8336a3a7ec46b
Weston Sankey <me@westonsankey.com>
2025-01-05 15:03:38 -0500

    fix scroll

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -38,7 +38,7 @@ export function Modal({
     <div className="fixed inset-0 bg-black dark:bg-opacity-50 bg-opacity-20 backdrop-blur-md flex justify-center items-center">
       <div
         ref={ref}
-        className="dark:bg-zinc-800 bg-white shadow-lg dark:bg-opacity-80 bg-opacity-70 dark:text-white px-8 pt-14 pb-8 md:rounded-lg relative md:w-[45rem] md:h-[40rem] w-full h-full rounded-none sm:overflow-y-auto"
+        className="dark:bg-zinc-800 bg-white shadow-lg dark:bg-opacity-80 bg-opacity-70 dark:text-white px-8 pt-14 pb-8 md:rounded-lg relative md:w-[45rem] md:h-[40rem] w-full h-full rounded-none overflow-y-auto"
       >
         <X
           className="absolute top-6 right-6 hover:cursor-pointer text-gray-500 dark:hover:text-white hover:text-gray-900"
