60b7e404d03c47c88baef39c94476298f965c327
Wes Sankey <me@westonsankey.com>
2025-01-18 14:40:22 -0500

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import { createProvider } from "@/api/services/hackernews";
 import { BestStories } from "./components/BestStories";
+import { promises as fs } from "fs";
+import path from "path";
 
 export default async function Home() {
   const provider = createProvider();
   const stories = await provider.getBestStories();
+
+  const filePath = path.join(process.cwd(), "comments.hbs");
+  const fileContent = await fs.readFile(filePath, "utf8");
+  console.log(fileContent);
 
   return (
     <div className="px-12 py-2 max-w-[1100px] mx-auto dark:text-white">
