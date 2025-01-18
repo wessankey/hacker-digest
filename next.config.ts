a1d462239350f1caccae938da24b549b31bfd70d
Wes Sankey <me@westonsankey.com>
2025-01-18 15:07:37 -0500

    test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/src/app/comments.hbs": ["./src/app/comments.hbs"],
+  },
 };
 
 export default nextConfig;
