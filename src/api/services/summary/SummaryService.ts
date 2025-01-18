93bb53904562f598731b0a0fc16331c7e0c8bdcb
Wes Sankey <me@westonsankey.com>
2025-01-18 15:18:12 -0500

    update max function duration

--- src/api/services/summary/SummaryService.ts
+++ src/api/services/summary/SummaryService.ts
@@ -11,6 +11,9 @@ import {
   TSummaryService,
 } from "./schema";
 
+export const maxDuration = 30;
+export const dynamic = "force-dynamic";
+
 Handlebars.registerHelper({
   json: function (context) {
     return JSON.stringify(context, null, 2);
