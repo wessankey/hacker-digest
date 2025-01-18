d9b9e23415085d2f55a530ae09dc60b25f37bdfd
Wes Sankey <me@westonsankey.com>
2025-01-18 14:47:49 -0500

    test

--- src/api/commentSummary.ts
+++ src/api/commentSummary.ts
@@ -8,6 +8,7 @@ export async function fetchCommentSummary(story: Story) {
   console.log("LOG:fetchCommentSummary");
   const provider = createProvider();
   const summaryService = createSummaryService();
+  console.log("LOG:created summary service");
   const cachedSummary = await summaryService.getCachedSummary(story.id);
   if (cachedSummary) return cachedSummary;
 
