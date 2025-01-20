715e57974732d197a1cf138199db7bfdc7693d1e
Wes Sankey <me@westonsankey.com>
2025-01-20 08:46:58 -0500

    add cron secret

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,7 +1,13 @@
-export const dynamic = "force-dynamic"; // static by default, unless reading the request
+export const dynamic = "force-dynamic";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function GET(request: Request) {
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
+
   console.log("log:Hello from ", process.env.VERCEL_REGION);
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
