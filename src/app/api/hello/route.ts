32f39636873c65043ef7ceed7ae3edcd4af685b1
Wes Sankey <me@westonsankey.com>
2025-01-20 16:05:47 -0500

    update cron api

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,12 +1,21 @@
+import { createProvider } from "@/api/services/hackernews";
+
 export const dynamic = "force-dynamic";
 
-export function GET(request: Request) {
+export async function GET(request: Request) {
   const authHeader = request.headers.get("authorization");
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return new Response("Unauthorized", {
       status: 401,
     });
   }
+
+  const provider = createProvider();
+  const stories = await provider.getBestStories();
+
+  stories.forEach((story) => {
+    console.log("log:story:", story.title);
+  });
 
   console.log("log:Hello from ", process.env.VERCEL_REGION);
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
