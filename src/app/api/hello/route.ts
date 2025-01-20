07c601318f3da09f708c54dc2a4f1e2fb6416651
Wes Sankey <me@westonsankey.com>
2025-01-20 08:40:43 -0500

    update api

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -2,5 +2,6 @@ export const dynamic = "force-dynamic"; // static by default, unless reading the
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function GET(request: Request) {
+  console.log("log:Hello from ", process.env.VERCEL_REGION);
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
